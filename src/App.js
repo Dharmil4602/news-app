845150812b08c76b3a97eeb41b00bae643066c08
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-20 00:51:17 +0530

    Created news and newsItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import NewsItem from './components/NewsItem';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello my first class based component</h1>
+       <Navbar/>
+       <News/>
+       {/* <NewsItem/> */}
       </div>
     )
   }
